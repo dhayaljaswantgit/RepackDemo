c0fac45b73255b276dd477f4a6b8ca76470ec810
Jaswant Dhayal <dhayaljaswant@gmail.com>
2024-02-23 13:03:04 +0530

    init all apps

---
+++ packages/app1/App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={{}}>
+      <Text style={styles.sectionTitle}>This is App1 </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default App;
---
+++ packages/host/App.tsx
@@ -0,0 +1,89 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {Federated} from '@callstack/repack/client';
+import React from 'react';
+import {Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
+
+import {ScriptManager, Script} from '@callstack/repack/client';
+
+const resolveURL = Federated.createURLResolver({
+  containers: {
+    app1: 'http://localhost:9000/[name][ext]',
+    app2: 'http://localhost:9001/[name][ext]',
+    serverHosted:
+      'https://firebasestorage.googleapis.com/v0/b/jaswant-dhayal.appspot.com/o/index.bundle?alt=media&token=2e13d9d9-3428-4545-b07c-ad8356a22698',
+    // module1: 'http://localhost:9002/[name][ext]',
+  },
+});
+
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
+  let url;
+  if (caller === 'main') {
+    url = Script.getDevServerURL(scriptId);
+  } else {
+    url = resolveURL(scriptId, caller);
+  }
+
+  if (!url) {
+    return undefined;
+  }
+
+  console.log({
+    scriptId,
+    caller,
+    url,
+  });
+
+  return {
+    url,
+    cache: false, // For development
+    query: {
+      platform: Platform.OS,
+    },
+  };
+});
+
+const App1 = React.lazy(() => Federated.importModule('app1', './App'));
+const App2 = React.lazy(() => Federated.importModule('app2', './App'));
+// const ServerHosted = React.lazy(() => Federated.importModule('app2', './App'));
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={{}}>
+      <Text style={styles.sectionTitle}>This is Host app Jassu</Text>
+      <React.Suspense fallback={<Text>Loading app1...</Text>}>
+        <App1 />
+      </React.Suspense>
+      <React.Suspense fallback={<Text>Loading app2...</Text>}>
+        <App2 />
+      </React.Suspense>
+      {/* <React.Suspense fallback={<Text>Loading app2...</Text>}>
+        <ServerHosted />
+      </React.Suspense> */}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    textAlign: 'center',
+    padding: 20,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
